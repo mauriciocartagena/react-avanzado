0b2f2225a0337ae2ee13174168f2038f680a2a07
Mauricio Cartagena <mc973220@gmail.com>
2020-06-02 16:18:59 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ListOfCategories } from './components/ListOfCategories';
+import { GlobalStyles } from './components/styles/GlobalStyles';
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => (
+  <div>
+    <GlobalStyles></GlobalStyles>
+    <Logo></Logo>
+    <ListOfCategories></ListOfCategories>
+    <ListOfPhotoCards></ListOfPhotoCards>
+  </div>
+)
 
-export default App;
---
+++ src/components/ListOfCategories/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Category } from '../Category';
+import { Item, List } from './styles';
+import { categories } from '../../api/db.json';
+
+export const ListOfCategories = () => {
+  return (
+    <List>
+      {
+        categories.map(category => <Item key={category.id}>
+          <Category {...category} />
+        </Item>)
+      }
+    </List>
+  )
+}
+
