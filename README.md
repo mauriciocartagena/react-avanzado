65c01fa3544ef2cbd444f90d46c39add4beb830e
Mauricio Cartagena <mc973220@gmail.com>
2020-06-04 09:38:57 -0400

    finish app web

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
 ## Available Scripts
+
+DEMO WEB  (:
+
+https://react-avanzado-e9b3sjq3t.now.sh/
 
 In the project directory, you can run:
 
--- cypress/integration/petgram/test_spec.js
+++ cypress/integration/petgram/test_spec.js
@@ -1,7 +1,24 @@
-/* global describe,it,expect*/
-describe('Mi primer test', function () {
-  it('para ver si funciona', () => {
-    expect(true).to.equal(false)
+/* global describe,it,cy*/
+describe('Petgram', function () {
+  it('para ver si la app funciona', () => {
+    cy.visit('/')
   });
+  it('Navegamos a unca categoria y vemos fotos', () => {
+    cy.visit('/pet/1')
+    cy.get('article')
+  });
+  it('si podemos navegar con la navbar a la home', () => {
+    cy.visit('/pet/1')
+    cy.get('nav a').first().click()
+    cy.url().should('include', '/')
+  });
+  it('los usuarios no registrados ven el formulario de registro e incio de sescion al ir a fav', () => {
+
+    cy.visit('/favs')
+    cy.get('form').should('have.length', 2)
+  });
+
+
+
 
 });
